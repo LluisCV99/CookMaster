af037a8193b58445a2fb5aac5af20bb89758f0c8
LluisCV99 <lluiscarrerasv@gmail.com>
2022-05-04 22:09:57 +0200

    NovaReceptaFragment crea un objecte receptes i guarda la info a GestorReceptes

--- app/src/main/java/com/example/cookmaster/ui/NovaRecepta/NovaReceptaFragment.java
+++ app/src/main/java/com/example/cookmaster/ui/NovaRecepta/NovaReceptaFragment.java
@@ -1,32 +1,73 @@
 package com.example.cookmaster.ui.NovaRecepta;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+
+import com.example.cookmaster.R;
 import com.example.cookmaster.databinding.NovaReceptaBinding;
+import com.example.cookmaster.ui.classes.Receptes;
+import com.example.cookmaster.ui.receptes.GestorReceptes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class NovaReceptaFragment extends Fragment {
 
     private NovaReceptaBinding binding;
+    private NovaReceptaViewModel novaReceptaViewModel;
+    GestorReceptes llista;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NovaReceptaViewModel novaReceptaViewModel =
-                new ViewModelProvider(this).get(NovaReceptaViewModel.class);
+        novaReceptaViewModel = new ViewModelProvider(this).get(NovaReceptaViewModel.class);
 
         binding = NovaReceptaBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        //novaReceptaViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText nomText = view.findViewById(R.id.nom_text);
+        EditText ingredientsText = view.findViewById(R.id.ingredients_text);
+        EditText preparacioText = view.findViewById(R.id.preparacio_text);
+        FloatingActionButton confirma = view.findViewById(R.id.floatingActionButton2);
+        try {
+        confirma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nomRecepta = nomText.getText().toString();
+                String ingredientsRecepta = ingredientsText.getText().toString();
+                String preparacioRecepta = preparacioText.getText().toString();
+                Receptes recepta = new Receptes(nomRecepta,
+                        ingredientsRecepta, preparacioRecepta, 1);
+                llista.add(recepta);
+
+
+                Toast.makeText(getContext(), nomRecepta, Toast.LENGTH_LONG).show();
+            }
+        });
+        }catch (Exception e){
+            Toast.makeText(getContext(), "ha petat", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 
     @Override
     public void onDestroyView() {
@@ -34,6 +75,4 @@ public class NovaReceptaFragment extends Fragment {
         binding = null;
     }
 
-    public void chose() {
-    }
 }
